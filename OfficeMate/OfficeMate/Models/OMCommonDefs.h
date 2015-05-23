1deecbcbef33255934373cf146c7aa5d73581784
Himadri Jyoti <himadri@bsb.in>
2015-05-23 12:56:43 +0530

    Added model files

---
+++ OfficeMate/OfficeMate/Models/OMCommonDefs.h
@@ -0,0 +1,13 @@
+//
+//  OMCommonDefs.h
+//  OfficeMate
+//
+//  Created by Himadri Jyoti on 23/05/15.
+//  Copyright (c) 2015 Ravindra Shetty. All rights reserved.
+//
+
+#ifndef OfficeMate_OMCommonDefs_h
+#define OfficeMate_OMCommonDefs_h
+
+
+#endif
---
+++ OfficeMate/OfficeMate/Models/OMModelManager.h
@@ -0,0 +1,20 @@
+//
+//  OMModelManager.h
+//  OfficeMate
+//
+//  Created by Himadri Jyoti on 23/05/15.
+//  Copyright (c) 2015 Ravindra Shetty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class OMUser;
+
+@interface OMModelManager : NSObject
+
++ (OMModelManager *)sharedManager;
+
+@property (nonatomic, strong, readonly) OMUser *currentUser;
+
+
+@end
